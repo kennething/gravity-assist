aa9608e04877c3b3a1306c0ceb692d258136b6d8
Kenneth Ng <145921389+DubNubz@users.noreply.github.com>
2024-12-11 13:14:33 -0500

    offset selection if code

--- components/Mail/Quill.client.vue
+++ components/Mail/Quill.client.vue
@@ -76,14 +76,18 @@ onMounted(async () => {
     const selection = quill.getSelection();
     if (!selection) return;
 
-    const content = defaultFormat(quill.getContents());
+    const [content, selectionOffset] = defaultFormat(quill.getContents());
     quill.setContents(content);
+    console.log(selectionOffset, selection.index);
+    selection.index -= selectionOffset;
+    console.log(selectionOffset, selection.index);
 
     const length = quill.getLength();
     const selectionIndex =
       delta.ops.find((op) => op.insert === "\n") || (oldDelta.ops[oldDelta.ops.length - 1].insert as string).includes("\n\n") || (oldDelta.ops.length === 1 && oldDelta.ops[0].insert === "\n")
         ? selection.index + 1
         : selection.index;
+    console.log(selection.index, selectionIndex);
     quill.setSelection({ index: length === 1 ? 0 : selectionIndex, length: 0 });
     emit("output", content);
 
@@ -105,6 +109,7 @@ function formatSelection(selectionRange: Range, selection: Record<string, unknow
   quill.format("color", color);
   quill.format("underline", selectionFormat.underline);
 
+  console.log(selectionFormat.underline, color);
   emit("event", Boolean(selectionFormat.underline), color);
 }
 
@@ -128,7 +133,9 @@ function format() {
   quill.format("color", props.color);
 }
 
-function defaultFormat(text: Delta) {
+function defaultFormat(text: Delta): [Delta, number] {
+  let selectionOffset = 0;
+
   for (let i = 0; i < text.ops.length; i++) {
     const op = text.ops[i];
     const insert = op.insert as string | undefined;
@@ -148,17 +155,29 @@ function defaultFormat(text: Delta) {
     const formattingRegex = /(#[rEeRODYGBUPWK]|#c[0-9a-fA-F]{6})/; // Convert IL formatting markers to quill
     if (insert && formattingRegex.test(insert)) {
       const [before, code, after] = insert.split(formattingRegex);
+      selectionOffset += code.length;
       if (before) text.ops.splice(i, 0, { insert: before, attributes: { ...op.attributes } });
-      
-      const textToInsert = (code === "#r" ? "\n" : "") + after;
-      const colorToInsert = code.length === 2 ? FormattingColors[code[1] as keyof typeof FormattingColors] : ("#" + code.slice(1));
-      text.ops.splice(i + 2, 0, { insert: textToInsert, attributes: { ...op.attributes, color: colorToInsert } });
-      text.ops.splice(i + 1, 1);
+      if (after) text.ops.splice(i + 2, 0, { insert: after, attributes: { ...op.attributes } });
+
+      if (code === "#r") op.insert = "\n";
+      else {
+        const colorToInsert = !["r", "e", "E"].includes(code[1])
+          ? code.length === 2
+            ? FormattingColors[code[1] as keyof typeof FormattingColors]
+            : "#" + code.slice(2)
+          : (op.attributes?.color ?? props.color);
+        const underline = op.attributes?.underline ? code !== "#e" : code === "#E";
+        emit("event", underline, colorToInsert as string);
+
+        text.ops.splice(i + 1, 1);
+      }
     }
 
     if (!op.attributes?.color && insert !== "\n") op.attributes = { ...op.attributes, color: "#ffffff" }; // Add fallback color
   }
-  return text;
+
+  console.log(text);
+  return [text, selectionOffset];
 }
 </script>
 
